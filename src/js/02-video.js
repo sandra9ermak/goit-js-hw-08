9d787756e583925ff7e8ddd95033198d5d50b841
sandra9ermak <sandra1ermak@gmail.com>
2021-10-01 18:10:52 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
 
+<<<<<<< Updated upstream
+=======
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(data => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+  }, 1000));
+
+let time = localStorage.getItem("videoplayer-current-time") || 0;
+
+player.setCurrentTime(time);
+>>>>>>> Stashed changes
